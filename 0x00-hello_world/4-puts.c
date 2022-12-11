dadcdadcd5edcd71358a413e604fa9683667f423
wangengi <wangengi.snr@gmail.com>
2022-12-11 06:42:42 -0800

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+/**
+ * main - To print a string using puts
+ * Return: 0
+ */
+init main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
