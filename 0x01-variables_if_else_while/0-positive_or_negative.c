3d481222cac4ff02a5aff3cee8ee901d41cf138a
wangengi <wangengi.snr@gmail.com>
2022-12-20 11:52:10 -0800

    Task 0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
-#include<stdlib.h>
-#include<time.h>
-#include<stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- * main - assign a random number to the variable n each time it is executed
- * print the random number and state whether it is positive, negative or zero
+ *  main - assign a random number to the variable n each time it is executed
+ *  print the random number and state whether it is positive, negative or zero
+ *  Return: always (0)
  */
 
 int main(void)
@@ -13,14 +13,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-
 	return (0);
 }
-Merge branch 'master' of https://github.com/wangengi/alx-zero_day
+
 
